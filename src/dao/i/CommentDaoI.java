932ea8e1871061b52e33bcceb5e951b4065580c0
Vixness <vix2018@gmail.com>
2017-04-13 14:55:52 +0800

    add comment dao interface

---
+++ src/dao/i/CommentDaoI.java
@@ -0,0 +1,76 @@
+package dao.i;
+
+import team.jiangtao.entity.Comment;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by tose on 2017/4/13.
+ */
+public interface CommentDaoI {
+    /**
+     *
+     * @param condition
+     * @param equalCondition
+     * @return comment
+     * @throws Exception
+     */
+    List<Comment> getCommentsByConditions(Map<String,Object> condition, boolean... equalCondition) throws Exception;
+
+    /**
+     *
+     * @param comment
+     * @return
+     * @throws Exception
+     */
+    Comment getCommentByPK(Comment comment)throws Exception;
+
+    /**
+     *
+     * @param comments
+     * @return comment
+     * @throws Exception
+     */
+    boolean addComments(List<Comment> comments)throws Exception;
+
+    /**
+     *
+     * @param comment
+     * @return flag of result
+     * @throws Exception
+     */
+    boolean addCommnet(Comment comment)throws Exception;
+
+    /**
+     *
+     * @param comments
+     * @return flag
+     * @throws Exception
+     */
+    boolean updateComments(List<Comment> comments)throws Exception;
+
+    /**
+     *
+     * @param comment
+     * @return flag
+     * @throws Exception
+     */
+    boolean updateComment(Comment comment)throws Exception;
+
+    /**
+     *
+     * @param comments
+     * @return flag
+     * @throws Exception
+     */
+    boolean deleteComments(List<Comment> comments)throws Exception;
+
+    /**
+     *
+     * @param comment
+     * @return flag
+     * @throws Exception
+     */
+    boolean deleteComent(Comment comment)throws Exception;
+}
