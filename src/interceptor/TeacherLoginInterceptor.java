0599afc9dc730b93c56a8a7abbe774aa6e6df611
codersdady <973634996@qq.com>
2017-05-19 11:26:22 +0800

    增加过滤器类，未实现类

---
+++ src/interceptor/TeacherLoginInterceptor.java
@@ -0,0 +1,15 @@
+package interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+/**
+ * Created by Administrator on 2017/5/18 0018.
+ */
+public class TeacherLoginInterceptor extends AbstractInterceptor{
+
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        return null;
+    }
+}
