ef72e006f7bf3f5e7c553c54ebb311bfd9eab479
Vixness <vix2018@gmail.com>
2017-05-23 19:33:24 +0800

    implentment interface

--- src/service/impl/AppealServicelmpl.java
+++ src/service/impl/AppealServicelmpl.java
@@ -1,7 +1,26 @@
 package service.impl;
 
+import service.i.AppealServiceI;
+import team.jiangtao.entity.Appeal;
+
+import java.util.List;
+
 /**
  * Created by tose on 2017/5/23.
  */
-public class AppealServicelmpl {
+public class AppealServicelmpl implements AppealServiceI {
+    @Override
+    public boolean addAppeals(List<Appeal> appeals) throws Exception {
+        return false;
+    }
+
+    @Override
+    public List<Appeal> getAppeals(int appealType) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean updateAppeal(Appeal appeal) throws Exception {
+        return false;
+    }
 }
--- src/service/impl/CommentServiceImpl.java
+++ src/service/impl/CommentServiceImpl.java
@@ -1,7 +1,32 @@
 package service.impl;
 
+import service.i.CommentServiceI;
+import team.jiangtao.entity.Comment;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by tose on 2017/5/23.
  */
-public class CommentServiceImpl {
+public class CommentServiceImpl implements CommentServiceI{
+    @Override
+    public List<Map<String, Double>> getScoreByType(Integer statType) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Comment> getAllComment(String tchId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean addComment(Comment comment) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean updateComent(Comment comment) throws Exception {
+        return false;
+    }
 }
