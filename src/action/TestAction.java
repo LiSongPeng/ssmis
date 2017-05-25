96885225215ee4fafa6e985185e04749a2964052
Vixness <vix2018@gmail.com>
2017-05-25 09:20:10 +0800

    add fastjson lib

--- src/action/TestAction.java
+++ src/action/TestAction.java
@@ -1,13 +1,20 @@
 package action;
 
+import com.alibaba.fastjson.JSONObject;
 import com.opensymphony.xwork2.ActionSupport;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import com.alibaba.fastjson.*;
+import team.jiangtao.entity.Appeal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tose on 2017/5/24.
@@ -19,6 +26,15 @@ import org.springframework.stereotype.Controller;
 public class TestAction extends ActionSupport {
     private String condition;
     private String json;
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
     public String getCondition() {
         return condition;
     }
@@ -29,7 +45,12 @@ public class TestAction extends ActionSupport {
 
     @Action(value = "hibernate",results = @Result(type = "json",params = {"root","json"}))
     public String testHibernate(){
-
+        Appeal appeal  = new Appeal();
+        appeal.setContent("a");
+        appeal.setDpmId("4");
+        appeal.setCrsId("12313");
+        json = JSON.toJSONString(appeal);
+//        System.out.println(json);
         return SUCCESS;
     }
 }
