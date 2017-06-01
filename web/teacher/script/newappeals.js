3b767448de9ea5c5c33016b762c1f317d5285a2a
Vixness <vix2018@gmail.com>
2017-06-01 19:44:37 +0800

    update web ui

--- web/teacher/script/newappeals.js
+++ web/teacher/script/newappeals.js
@@ -19,7 +19,8 @@ function getAppeals(type) {
                 var tchId = n.tchId;
                 var stuName = n.studentByStuId.name;
                 var stuId = n.stuId;
-                var stuGrade = n.studentByStuId.grade;
+                var stuGrade = "女";
+                if(n.studentByStuId.grade=='1') stuGender = "男";
                 var stuGender = n.studentByStuId.gender;
                 var stuclassNo = n.studentByStuId.classNo;
                 var date = new Date(n.date);
@@ -31,9 +32,22 @@ function getAppeals(type) {
                     '<td><span class="mdl-chip"><span class="mdl-chip__text">'+tchName+"("+tchId+')</span></span></td>' +
                     '<td><span class="mdl-chip"><span class="mdl-chip__text">'+date+'</span></span></td>' +
                     '</tr>' +
-                    '<tr style="display:none"><td colspan="6"><div style="width: auto;height: 320px;background-color: #00acc1;margin-left: auto;margin-right: auto">' +
-                    '' +
-                    '<button class="mdl-button mdl-js-button mdl-button--icon"> <i class="material-icons">more_vert</i> </button>' +
+                    '<tr style="display:none"><td colspan="6">' +
+                    '<div class="mdl-card mdl-shadow--2dp through mdl-shadow--16dp" style="width:auto" align="left"><div>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">S</span><span class="mdl-chip__text">'+stuName+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">S</span><span class="mdl-chip__text">'+stuId+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">S</span><span class="mdl-chip__text">'+stuGrade+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">S</span><span class="mdl-chip__text">'+stuclassNo+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">S</span><span class="mdl-chip__text">'+stuGender+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">D</span><span class="mdl-chip__text">'+dpmName+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">D</span><span class="mdl-chip__text">'+dpmId+'</span></span><br/>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">C</span><span class="mdl-chip__text">'+crsName+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">C</span><span class="mdl-chip__text">'+crsId+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">T</span><span class="mdl-chip__text">'+tchName+'</span></span>' +
+                    '<span class="mdl-chip mdl-chip--contact"><span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">T</span><span class="mdl-chip__text">'+tchId+'</span></span>' +
+                    '</div><div class="mdl-card__supporting-text" align="left">' +content+ '</div>' +
+                    '<div mdl-card__actions mdl-card--border><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" style="width: 96px">回复</button></div>' +
+                    '<div class="mdl-card__menu"><button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"><i class="material-icons">more_vert</i></button></div>' +
                     '</div></td></tr>';
                 $("#ap_table").append(tr);
                 // console.log(content+" "+crsId+" "+crsName+" "+dpmName+" "+dpmId+" "+ tchId+" "+tchName);
