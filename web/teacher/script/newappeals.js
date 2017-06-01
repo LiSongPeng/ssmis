f166c9c113c010886be73511f49518d81ca8abc8
Vixness <vix2018@gmail.com>
2017-06-01 10:14:31 +0800

    update web

--- web/teacher/script/newappeals.js
+++ web/teacher/script/newappeals.js
@@ -9,6 +9,31 @@ function getAppeals(type) {
             $("#np_tag").attr("data-badge",list.length);
             $("#np_tag").show();
             $("#zone").show();
+            console.log(list[3]);
+            $.each(list,function(i,n){
+                var content = n.content;
+                var crs = n.courseByCrsId;
+                var crsName = crs.crsName;
+                var crsId = n.crsId;
+                var departmentByDpmId = n.departmentByDpmId
+                var dpmName = departmentByDpmId.dpmName;
+                var dpmId = n.dpmId;
+                var tch = departmentByDpmId.teachersByDpmId[0];
+                var tchName = tch.name;
+                var tchId = n.tchId;
+                var tr = "<tr>" +
+                    "<td></td>" +
+                    "<td></td>" +
+                    "<td></td>" +
+                    "<td></td>" +
+                    "<td></td>" +
+                    "<td></td>" +
+                    "<td></td>" +
+                    "</tr>";
+                $("#ap_table").append(tr);
+                // console.log(crs);
+                //console.log(content+" "+crsId+" "+crsName+" "+dpmName+" "+dpmId+" "+ tchId+" "+tchName);
+            })
         }
     })
 }
